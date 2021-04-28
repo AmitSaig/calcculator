aa18efd17285a98aa36e6e284b81997251c68a69
Amitsaig <amitsa1995@gmail.com>
2021-04-28 18:02:56 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Calulator</h1>
+        <h1>React Calculator</h1>
         <Calculator/>
       </header>
       <Socials/>
